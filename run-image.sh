a0ee97032a2c1392a162776a3de668d9f8a81774
Lawrence Dang <lawrend1@uci.edu>
2020-09-11 14:53:53 -0700

    Added github and docker hub credential entries to yaml

--- run-image.sh
+++ run-image.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-# Usage: ./run-image.sh <image-name> [jenkins port [agent port]]
-docker run -it -p ${2:-8080}:8080 -p ${3:-50000}:50000 -v /var/run/docker.sock:/var/run/docker.sock $1
+# Usage: ./run-image.sh <image-name> <github token env> <docker hub username env> <docker hub token env> [jenkins port [agent port]]
+docker run -it -e \"$2\" -e \"$3\" -e \"$4\" -p ${5:-8080}:8080 -p ${6:-50000}:50000 -v /var/run/docker.sock:/var/run/docker.sock $1

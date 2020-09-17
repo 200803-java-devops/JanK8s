ff0f28c01c738da883711824feb438e147d01e9e
Douglas Liu <you@example.com>
2020-09-17 13:21:42 -0700

    Service Account Added

--- Slaves/DeliveryServer/server.js
+++ Slaves/DeliveryServer/server.js
@@ -1,32 +1,7 @@
 var http = require('http');
 const { exec } = require("child_process");
-
 http.createServer(function (req, res) {
-  exec("docker pull jank8s/jenkins-sample", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
-  
-  exec("docker kill sample1", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
-
-  exec("docker run --rm --name=sample1 -p 8085:8080 jank8s/jenkins-sample", (error, stdout, stderr) => {
+  exec("kubectl delete deployment sample -n jank8s || true && kubectl apply -f sample-deploy.yaml -n jank8s", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
@@ -38,8 +13,7 @@ http.createServer(function (req, res) {
     console.log(`stdout: ${stdout}`);
   });
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('running pull command');
+  res.write("Attempting to create deployment for sample");
   res.end();
 }).listen(8086);
-
-console.log("listing on port 8086")
+console.log("listening on port 8086")
